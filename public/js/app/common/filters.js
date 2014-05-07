ac6cc35abfa2b56630337cf92065de7590c4d53c
Alexey Volkov <alexey.wild88@gmail.com>
2014-05-07 23:39:00 +0300

    fix

---
+++ public/js/app/common/filters.js
@@ -0,0 +1,28 @@
+(function() {
+
+    'use strict';
+
+    /* Services */
+    angular.module('regidiumApp')
+        .filter('orderObjectBy', function() {
+          return function(items, field, reverse) {
+            var filtered = [];
+
+            angular.forEach(items, function(item) {
+                filtered.push(item);
+            });
+
+            filtered.sort(function (a, b) {
+                return (a[field] > b[field]);
+            });
+
+            if(reverse) {
+                filtered.reverse();
+            }
+
+            return filtered;
+          };
+        })
+    ;
+
+})();
