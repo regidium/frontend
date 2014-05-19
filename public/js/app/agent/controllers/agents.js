f18ed04657858e9569d7f43e7f11eb28ceddcb11
Alexey Volkov <alexey.wild88@gmail.com>
2014-05-19 11:44:39 +0300

    fix

--- public/js/app/agent/controllers/agents.js
+++ public/js/app/agent/controllers/agents.js
@@ -108,6 +108,7 @@ function AgentAgentsCtrl($rootScope, $scope, $http, $log, $fileUploader, flash, 
         // Добавляем обрабочик загрузки файла
         uploader.bind('success', function (event, xhr, item, response) {
             if (response && response.url) {
+                $scope.current_agent.avatar = '';
                 $scope.current_agent.avatar = response.url;
             }
         });
