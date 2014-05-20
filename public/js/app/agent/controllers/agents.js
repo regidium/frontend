cf705b040388791642189426114d20542bc6a1a0
Alexey Volkov <alexey.wild88@gmail.com>
2014-05-20 11:09:48 +0300

    fix

--- public/js/app/agent/controllers/agents.js
+++ public/js/app/agent/controllers/agents.js
@@ -113,8 +113,8 @@ function AgentAgentsCtrl($rootScope, $scope, $http, $log, $fileUploader, flash, 
         // Добавляем обрабочик загрузки файла
         uploader.bind('success', function (event, xhr, item, response) {
             if (response && response.url) {
-                $('#avatar').attr('url', '');
-                $('#avatar').attr('url', response.url);
+                $('#avatar').attr('src', '');
+                $('#avatar').attr('src', response.url);
                 $scope.t = (+new Date);
                 $scope.current_agent.avatar = response.url;
                 flash.success = 'Agent avatar uploaded';
